51ca9035317a157f45bd99ed5a4a3c05d33282f5
Jeferson Cieza <jcieza@sudtechnologies.com>
2019-10-19 01:06:13 -0500

    Initial setup with PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ButtonModule} from 'primeng/button';
+import {PanelMenuModule} from 'primeng/panelmenu';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ButtonModule,
+    PanelMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
